12d88bab5a36b924f3f0f180be9af1387c136b1e
GitHubAlexM <alexander@alexo.xyz>
2015-10-23 18:01:19 -0400

    updated project. assignment 2 begins here lol. player added with animation included. more game notes.

---
+++ asgmt_2/Assets/Scripts/playerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+
+	public Animator anim;
+
+
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKey (KeyCode.RightArrow)) {
+			anim.SetBool ("walking", true);
+		} else {
+			anim.SetBool ("walking", false);
+		}
+	}
+}
